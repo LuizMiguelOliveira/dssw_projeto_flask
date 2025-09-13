cfcdd1135b979224d0e4928e1c5b768f64f6578e
Luiz Miguel Oliveira de Amorim <Luizmiguel@academicos.com>
2025-09-13 16:34:09 -0300

    Preparação do fronte

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def ola():
-    return ('<h1> Olá Mundo! '
-            '</h1>')
+    #return ('<h1> Olá Mundo! </h1>')
+    return render_template("index.html")
 
 app.run()
 
